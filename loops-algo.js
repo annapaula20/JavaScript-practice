77b5e847407168ac9d22dd137e3ae384213b9684
annapaula_d <annapaula.desouza1@gmail.com>
2021-10-09 00:03:09 -0400

    Initial commit

---
+++ loops-algo.js
@@ -0,0 +1,13 @@
+
+
+for (let i = 0; i < 20; i++) {
+    if (i % 2 !== 0)
+        console.log(i);
+}
+
+// num and sum
+var sum = 0;
+for (let i = 0; i < 5; i++) {
+    sum += i
+    console.log('num:', i, 'sum:', sum)
+}
