2c26c01943f8312dde579574b5efbf098f064827
annapaula_d <annapaula.desouza1@gmail.com>
2022-06-19 23:17:03 -0400

    Initial commit

---
+++ dice-roll-func.js
@@ -0,0 +1,14 @@
+function rollDie() {
+    let roll = Math.floor(Math.random() * 6) + 1;
+    console.log('Rolled : ${roll}');
+}
+
+function throwDice() {
+    rollDie();
+    rollDie();
+    rollDie();
+    rollDie();
+    rollDie();
+    rollDie();
+    rollDie();
+}
